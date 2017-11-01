f05ab58171964f9a8d96b1dc68fde876dcdf431d
umarfarisi <pharese@gmail.com>
2017-11-01 17:25:24 +0700

    done with tutorial

---
+++ Tutorial07Producer/src/main/java/com/tutorial/lima/rest/StudentRestController.java
@@ -0,0 +1,23 @@
+package com.tutorial.lima.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tutorial.lima.model.StudentModel;
+import com.tutorial.lima.service.StudentService;
+
+@RestController
+@RequestMapping("/rest")
+public class StudentRestController {
+
+	@Autowired
+	StudentService studentService;
+	
+	@RequestMapping("/student/view/{npm}")
+	public StudentModel view(@PathVariable(value = "npm") String npm) {
+		return studentService.selectStudent(npm);
+	}
+	
+}
