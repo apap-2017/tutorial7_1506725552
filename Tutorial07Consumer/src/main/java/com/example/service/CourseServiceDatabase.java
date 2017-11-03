3b8e823d2f2bd036f22d7dca17d230b5618ca9fa
umarfarisi <pharese@gmail.com>
2017-11-03 17:51:52 +0700

    fix error

--- Tutorial07Consumer/src/main/java/com/example/service/CourseServiceDatabase.java
+++ Tutorial07Consumer/src/main/java/com/example/service/CourseServiceDatabase.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,12 @@ public class CourseServiceDatabase implements CourseService{
 	@Override
 	public CourseModel selectCourse(String idCourse) {
 		return courseMapper.selectCourse(idCourse);
+	}
+
+	@Override
+	public List<CourseModel> selectAllCourse() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
